dd5551042a8e6e030f837b5bc049ab6ded48a06e
iewher <goshagang@mail.ru>
2023-11-09 18:42:48 +0500

    added README.md

--- index.ts
+++ index.ts
@@ -2,9 +2,23 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 const response = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const new_page = await browser.newPage();
-    new_page.goto('https://hh.ru/')
-}
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.goto("https://hh.ru/");
+  await page.waitForSelector(".supernova-button"); // Супер важно
+
+  //TODO: Имеем две кнопки с одинаковыми классами, забирает первую, нужно забрать вторую
+
+  const login_button = await page.evaluate(() => {
+    const button = document.querySelector(
+      ".supernova-navi-item.supernova-navi-item_lvl-2.supernova-navi-item_button.supernova-navi-item_no-mobile.supernova-navi-item_dashboard"
+    );
+    return button ? button.textContent : null;
+  });
+
+  console.log(login_button);
+
+  await browser.close();
+};
 
 response();
