3ae4dd7f4d71f912db5eabe10485549451185b0a
iewher <goshagang@mail.ru>
2023-11-10 21:10:37 +0500

    take away the login button

--- index.ts
+++ index.ts
@@ -4,17 +4,20 @@ const fs = require("fs");
 const response = async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  page.goto("https://hh.ru/");
-  await page.waitForSelector(".supernova-button"); // Супер важно
 
-  //TODO: Имеем две кнопки с одинаковыми классами, забирает первую, нужно забрать вторую
+  await page.goto("https://hh.ru/");
 
-  const login_button = await page.evaluate(() => {
-    const button = document.querySelector(
-      ".supernova-navi-item.supernova-navi-item_lvl-2.supernova-navi-item_button.supernova-navi-item_no-mobile.supernova-navi-item_dashboard"
-    );
-    return button ? button.textContent : null;
-  });
+  await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+
+  await page.waitForSelector(".supernova-button");
+
+  const login_button = await page.evaluate((sel) => {
+    let elements = Array.from(document.querySelectorAll(sel));
+    let responses = elements.map((element) => {
+      return element.innerText;
+    });
+    return responses;
+  }, ".supernova-navi-item.supernova-navi-item_lvl-2.supernova-navi-item_button.supernova-navi-item_no-mobile.supernova-navi-item_dashboard");
 
   console.log(login_button);
 
